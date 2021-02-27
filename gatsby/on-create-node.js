3458f54c0e49fcfe2d59a406288565c597fcf853
tonooo71 <mrtm.conv.tky@gmail.com>
2021-02-28 00:11:44 +0900

    rebuilding site Sun 28 Feb 2021 12:11:44 AM JST

--- gatsby/on-create-node.js
+++ gatsby/on-create-node.js
@@ -7,21 +7,21 @@ const onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
   if (node.internal.type === "MarkdownRemark") {
-    if (typeof node.frontmatter.slug !== "undefined") {
-      const dirname = getNode(node.parent).relativeDirectory;
-      createNodeField({
-        node,
-        name: "slug",
-        value: `/${dirname}/${node.frontmatter.slug}`,
-      });
-    } else {
-      const value = createFilePath({ node, getNode });
-      createNodeField({
-        node,
-        name: "slug",
-        value,
-      });
-    }
+    // if (typeof node.frontmatter.slug !== "undefined") {
+    //   const dirname = getNode(node.parent).relativeDirectory;
+    //   createNodeField({
+    //     node,
+    //     name: "slug",
+    //     value: `/${dirname}/${node.frontmatter.slug}`,
+    //   });
+    // } else {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      node,
+      name: "slug",
+      value,
+    });
+    // }
 
     if (node.frontmatter.tags) {
       const tagSlugs = node.frontmatter.tags.map(
