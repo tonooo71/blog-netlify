ba91013a62e36d8723c16f3f5981c5d2c7bcee80
tonooo71 <mrtm.conv.tky@gmail.com>
2021-03-12 22:28:03 +0900

    rebuilding site Fri 12 Mar 2021 10:27:59 PM JST

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-git add .
-
 msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
 
-git push origin master
-
-# for WSL
-rm -rf reduxcache*
-
+# submodule
 cd githubio
 ls | xargs rm -rf
 cp -r ../public/* ./
@@ -24,3 +14,17 @@ fi
 git commit -m "$msg"
 
 git push origin master
+cd ..
+
+# module
+git add .
+
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
+
+# for WSL
+rm -rf reduxcache*
